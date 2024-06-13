621ff4dc44a2bd8325e431a93ca588e197cb533f
Cristian <cdom127@wgu.edu>
2024-06-12 21:36:54 -0700

    Setup public routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Landing from './routes/public/Landing';
+import Approach from './routes/public/Approach';
+import Vision from './routes/public/Vision';
+import Properties from './routes/public/Properties';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />,
+    children: [
+      {
+        path: 'approach',
+        element: <Approach />,
+      },
+      {
+        path: 'vision',
+        element: <Vision />,
+      },
+      {
+        path: 'properties',
+        element: <Properties />,
+      },
+    ],
+  },
+  //admin routes
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
