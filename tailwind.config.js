427cec85aef3b13829daf732dffaee7b48421bb7
Cristian <cdom127@wgu.edu>
2024-06-13 08:29:09 -0700

    Add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-primary': '#fffcf2',
+        'bg-secondary': '#ccc5b9',
+        'content-primary': '#1e1d1b',
+        'content-secondary': '#403d39',
+        'brand-accent': 'eb5e28',
+      },
+    },
   },
   plugins: [],
 };
