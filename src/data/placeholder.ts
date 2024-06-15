d762ea5cba6559423e8824c66e81b57db10258c8
Cristian <cdom127@wgu.edu>
2024-06-15 10:07:34 -0700

    Sample data to construct property pages

---
+++ src/data/placeholder.ts
@@ -0,0 +1,162 @@
+export const properties = [
+  {
+    pid: 48181220,
+    address: {
+      street: '2035 Toboggan Way',
+      zipcode: '92154',
+      city: 'San Diego',
+      state: 'CA',
+      latitude: 32.57528,
+      longitude: -117.04722,
+    },
+    cost: 899999,
+    bedrooms: 5,
+    bathrooms: 2.5,
+    property_size: '1,992 sqft',
+    lot_size: '4,522 sqft',
+    overview: `Welcome to your dream home in the charming Nestor community, just moments from the picturesque Imperial Beach. This newly remodeled gem boasts 5 bedrooms, 2.5 bathrooms, and spans a comfortable 2000 square feet, offering ample space for your family's needs. Step inside and be greeted by the epitome of modern luxury. The entire home has been adorned with high-end finishes, including new luxury vinyl plank flooring that exudes both elegance and durability. The heart of this home lies in its stunning kitchen, where every detail has been carefully curated. New cabinets provide ample storage space, while quartz countertops offer both style and functionality. Equipped with top-of-the-line stainless steel appliances, this kitchen is a chef's paradise, ready to inspire culinary creations. The bathrooms have been tastefully remodeled, boasting elegant finishes that elevate every moment of self-care. Whether you're unwinding in the master suite or refreshing in the guest bath, every corner exudes sophistication and comfort. Step outside into your expansive yard, where lush turf creates the perfect oasis for both pets and family gatherings. With ample space for outdoor activities and entertaining, this yard is destined to become your personal sanctuary. Located just minutes from the beach, Silver Strand, and Coronado, this home offers unparalleled access to Southern California's most coveted coastal destinations. Additionally, its convenient proximity to the freeway ensures effortless travel to nearby attractions. With shopping, dining, and nightlife options just close by, this home truly offers the best of both worlds: a tranquil retreat nestled in the heart of a vibrant community. Don't miss your chance to make this exquisite property your own slice of paradise.`,
+    home_type: 'House',
+    year_built: 1980,
+    whats_special: [
+      'Newly remodeled',
+      'Expansive yard',
+      'Stunning kitchen',
+      'Quartz countertops',
+      'High-end finishes',
+      'Top-of-the-line stainless steel appliances',
+      'New cabinets',
+      'Lush turf',
+    ],
+    photo_gallery: [
+      'https://photos.zillowstatic.com/fp/1.jpg',
+      'https://photos.zillowstatic.com/fp/2.jpg',
+      'https://photos.zillowstatic.com/fp/3.jpg',
+    ],
+    facts: {
+      interior: {
+        bed_bath: [
+          'Bedrooms: 5',
+          'Bathrooms: 2.5',
+          'Full bathrooms: 2',
+          'Half bathrooms: 1',
+        ],
+        cooling: ['Central Air'],
+        appliances: ['Laundry: Gas & Electric Dryer Hookup, In Garage'],
+        features: [
+          'Kitchen, Living Room, Master Bathroom, Master Bedroom',
+          'Fireplace in family room',
+        ],
+        interior_area: ['Total interior livable area: 1,992 sqft'],
+      },
+      property: {
+        parking: [
+          'Total spaces: 2',
+          'Parking features: Garage - Attached',
+          'Garage spaces: 2',
+          'Covered spaces: 2',
+        ],
+        property: ['Two story'],
+        lot: [
+          'Lot size: 4,522 sqft',
+          'Lot features: 0-1 Unit/Acre, Front Yard, Level with Street',
+        ],
+        other: ['Parcel number: 6333830400', 'Zoning: R-1:SINGLE FAM-RES'],
+      },
+      construction: {
+        type: ['Home type: Single Family Residence'],
+        condition: ['Year built: 1980'],
+      },
+      utils: {
+        utilities: ['Sewer information: Public Sewer'],
+      },
+      community: {
+        community: ['Community features: Curbs, Sidewalks'],
+        location: ['Region: San Diego'],
+      },
+    },
+  },
+  {
+    pid: 16854891,
+    address: {
+      street: '6106 Camino De La Costa',
+      zipcode: '92037',
+      city: 'La Jolla',
+      state: 'CA',
+      latitude: 32.84306,
+      longitude: -117.25556,
+    },
+    cost: 1625000,
+    bedrooms: 5,
+    bathrooms: 4.5,
+    property_size: '5,674 sqft',
+    lot_size: '0.39 Acres',
+    overview:
+      'Experience ocean front living at its finest from this prime property located on perhaps the most coveted street in La Jolla. Modernist in style, blending raw concrete, teak wood, metal and glass converging with carefully curated architectural artifacts, create this one-of-a-kind residence.  Offering an intimate relationship with the ocean, the positioning on the bluff could never be duplicated. Expansive south coast views and beyond along with year-round sunsets and accessible sandy coves below, deliver both a sublime living experience and a seamless connection to the natural marine environment.',
+    home_type: 'House',
+    year_built: 2005,
+    whats_special: [
+      'Oceanfront location',
+      'Gourmet kitchen with high-end appliances',
+      'Hardwood floors throughout',
+      'Spa-like master bathroom with ocean views',
+      'Infinity pool and hot tub',
+      'Private beach access',
+    ],
+    photo_gallery: [
+      'https://photos.zillowstatic.com/fp/1.jpg',
+      'https://photos.zillowstatic.com/fp/2.jpg',
+      'https://photos.zillowstatic.com/fp/3.jpg',
+    ],
+    facts: {
+      interior: {
+        bed_bath: [
+          'Bedrooms: 3',
+          'Bathrooms: 5',
+          'Full bathrooms: 3',
+          'Half bathrooms: 2',
+        ],
+        cooling: ['Central Forced Air'],
+        appliances: [
+          'Included: Dishwasher, Disposal, Dryer, Refrigerator, Washer, 6 Burner Stove, Built-In Range',
+          'Laundry: Laundry Room, Electric',
+        ],
+        features: [
+          'Flooring: Stone, Wood',
+          'Basement: Walk-Out Access',
+          'Number of fireplaces: 5',
+        ],
+        interior_area: ['Total interior livable area: 5,674 sqft'],
+      },
+      property: {
+        parking: [
+          'Total spaces: 5',
+          'Parking features: Garage: Attached, Non-Garage: Driveway - Concrete',
+          'Attached garage spaces: 4',
+          'Has uncovered spaces: Yes',
+        ],
+        property: [
+          'Two story',
+          'Oceanfront location',
+          'View description: City, Evening Lights, Ocean, Panoramic, Panoramic Ocean, Other/Remarks, Rocks, Water, Bluff, Coastline, White Water, City Lights',
+          'Oceanfront description: Ocean, Panoramic Ocean, Water, Coastline, White Water',
+        ],
+        lot: ['Size: 0.39 Acres', 'Topography: Level,Gently Rolling'],
+        other: ['Parcel number: 3571410600'],
+      },
+      construction: {
+        type: [
+          'Home type: Single Family Residence',
+          'Architectural style: Contemporary, Custom Built, Modern, See Remarks',
+        ],
+        condition: ['Year built: 2005'],
+      },
+      utils: {
+        utilities: ['Water: Public', 'Sewer: Sewer Connected'],
+      },
+      community: {
+        community: ['Community features: Curbs, Sidewalks'],
+        location: ['Region: La Jolla', 'Subdivision: Lower Hermosa'],
+      },
+    },
+  },
+];
