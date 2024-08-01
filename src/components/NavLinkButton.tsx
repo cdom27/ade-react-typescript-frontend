6e0fe21065f1ffff3028ab287ba74ecd8e32c701
Cristian <cdom127@wgu.edu>
2024-07-31 19:00:40 -0700

    Reusable link component with tailwind animation

---
+++ src/components/NavLinkButton.tsx
@@ -0,0 +1,37 @@
+import { NavLink } from 'react-router-dom';
+import { Arrow } from './Icons';
+
+interface NavLinkButtonProps {
+  className: string;
+  text: string;
+  bgClasses: string;
+  arrowClasses: string;
+  to: string;
+}
+
+const NavLinkButton = ({
+  className,
+  text,
+  bgClasses,
+  arrowClasses,
+  to,
+}: NavLinkButtonProps) => {
+  return (
+    <NavLink
+      to={to}
+      className={`group relative overflow-hidden font-fraktion_reg flex items-center justify-between text-md border-[2px] rounded-full px-9 py-4 transition-colors duration-300 ease-in-out ${
+        className || ''
+      }`}
+    >
+      <span className="relative z-10">{text}</span>
+      <Arrow
+        className={`size-5 relative z-10 transition-colors duration-300 ease-in-out ${arrowClasses}`}
+      />
+      <div
+        className={`absolute inset-0 transform translate-y-full transition-transform duration-300 ease-in-out group-hover:translate-y-0 ${bgClasses}`}
+      />
+    </NavLink>
+  );
+};
+
+export default NavLinkButton;
