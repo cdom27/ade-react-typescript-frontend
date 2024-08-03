c345a98a978aa2a17e876e73694ce252f2f50f46
Cristian <cdom127@wgu.edu>
2024-08-02 19:54:53 -0700

    Updated footer

--- src/components/Navigation/Footer.tsx
+++ src/components/Navigation/Footer.tsx
@@ -1,70 +1,43 @@
 import { NavLink } from 'react-router-dom';
-
-const links = [
-  {
-    href: '/vision',
-    name: 'Vision',
-  },
-  {
-    href: '/approach',
-    name: 'Approach',
-  },
-  {
-    href: '/homes',
-    name: 'Homes',
-  },
-  {
-    href: '/contact',
-    name: 'Contact',
-  },
-];
-
-const contactInfo = [
-  {
-    title: 'Phone',
-    text: '6199640960',
-  },
-  {
-    title: 'Email',
-    text: 'info@ade.com',
-  },
-  {
-    title: 'Office',
-    text: '80 Genesee Avenue San Diego, CA 90192',
-  },
-];
+import { links } from '../../utils/navUtils';
+import { Arrow } from '../Icons';
 
 const Footer = () => {
   return (
-    <footer className="space-y-12 pt-28 pb-20 px-6 bg-bg-primary text-content-primary border-t-2 border-bg-secondary">
-      <div>
-        <h2 className="text-3xl pb-6 font-bold">Get in Touch</h2>
-        {contactInfo.map((info) => (
-          <div key={info.title} className="pb-4">
-            <h3 className="text-2xl font-semibold">{info.title}</h3>
-            <h4 className="text-xl font-medium underline">{info.text}</h4>
-          </div>
-        ))}
-      </div>
-      <div>
-        <h2 className="text-3xl pb-6 font-bold">Learn More</h2>
+    <footer className="px-6 py-20 text-center bg-leafs bg-cover">
+      <h2 className="text-4xl font-editorial_ul_italic text-primary">
+        Embrace San Diego With Ade
+      </h2>
+      <ul className="flex flex-col gap-y-4 mt-16">
         {links.map((link) => (
-          <h3 key={link.href} className="text-2xl pb-1 underline">
+          <li key={link.name}>
             <NavLink
               to={link.href}
               className={({ isActive }) =>
-                `${
-                  isActive
-                    ? 'text-content-primary'
-                    : 'text-bg-secondary hover:text-brand-accent active:text-brand-accent'
-                } font-medium`
+                ` font-editorial_ul text-2xl ${
+                  isActive ? 'text-accent' : 'text-primary'
+                }`
               }
             >
               {link.name}
             </NavLink>
-          </h3>
+          </li>
         ))}
-      </div>
+        <li>
+          <a
+            href="/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group relative overflow-hidden font-fraktion_reg flex items-center justify-between text-md rounded-full px-9 py-4 transition-colors duration-300 ease-in-out text-content bg-accent hover:bg-primary active:bg-primary mt-8"
+          >
+            <span className="relative z-10">
+              Learn How This Website Was Made
+            </span>
+            <Arrow className="size-7 relative z-10 transition-colors duration-300 ease-in-out stroke-content rotate-[-45deg]" />
+            <div className="absolute inset-0 transform translate-y-full transition-transform duration-300 ease-in-out group-hover:translate-y-0 bg-primary" />
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 };
