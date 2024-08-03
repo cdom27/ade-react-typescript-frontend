e773fd92b273b21b6613897007ec584da648bf1d
Cristian <cdom127@wgu.edu>
2024-08-03 12:02:46 -0700

    Card for process gallery

---
+++ src/components/ProcessCard.tsx
@@ -0,0 +1,22 @@
+interface ProcessCardProps {
+  title: string;
+  children: React.ReactNode;
+  imgUrl: string;
+  imgClassName?: string;
+}
+
+const ProcessCard = ({ title, children, imgUrl }: ProcessCardProps) => {
+  return (
+    <div className="text-left">
+      <img
+        src={imgUrl}
+        alt={title}
+        className="rounded-xl object-cover h-[300px] w-full"
+      />
+      <h3 className="text-3xl font-editorial_ul pt-4 text-left">{title}</h3>
+      <div className="pt-4 space-y-4">{children}</div>
+    </div>
+  );
+};
+
+export default ProcessCard;
