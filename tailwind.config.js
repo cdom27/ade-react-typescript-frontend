c726bbb8d2d4f264b2de1266c099b7c9d404b69b
Cristian <cdom127@wgu.edu>
2024-08-15 10:28:52 -0700

    media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+        '3xl': '1920px',
+        '4xl': '2560px',
+      },
       colors: {
         primary: '#FDFDF1',
         content: '#39442B',
