1688c8ea382ecfecfa0ae32f9fde7eb9685c98c7
Cristian <cdom127@wgu.edu>
2024-12-09 11:55:40 -0800

    Update spacing for flex-grow. Align details and view button to bottom of card div for consistency.

--- src/components/HomeCard/HomeCard.tsx
+++ src/components/HomeCard/HomeCard.tsx
@@ -17,38 +17,37 @@ const HomeCard = ({
   overview,
 }: Props) => {
   return (
-    <div className="flex flex-col flex-grow">
+    <div className='flex flex-col flex-grow'>
       <NavLink to={`/homes/${id}`}>
         <img
           src={mainImgUrl}
           alt={'Image for: ' + address}
-          loading="eager"
-          decoding="auto"
-          role="img"
+          loading='eager'
+          decoding='auto'
+          role='img'
           aria-label={'Image for: ' + address}
-          className="rounded-xl object-cover xl:h-[366px] 2xl:w-full"
+          className='rounded-xl object-cover xl:h-[366px] 2xl:w-full'
         />
       </NavLink>
 
       <NavLink to={`/homes/${id}`}>
-        <h4 className="text-2xl pt-2 font-editorial_ul underline sm:text-3xl">
+        <h4 className='text-2xl pt-2 font-editorial_ul underline sm:text-3xl'>
           {address}
         </h4>
       </NavLink>
 
-      <p className="mt-4 text-md sm:text-xl">{overview}</p>
+      <p className='mt-4 text-md sm:text-xl'>{overview}</p>
 
-      <ul className="grid grid-cols-2 font-haas_md text-lg gap-x-3 mt-4">
-        <li>${cost.toLocaleString()}</li>
-        <li>Built {yearBuilt}</li>
-        <li>{livableAreaSize} home</li>
-        <li>{bedrooms} Bed</li>
-        <li>{lotSize} lot</li>
-        <li>{bathrooms} Bath</li>
-      </ul>
-
-      <div className="mt-8 sm:mt-auto">
-        <NavButton to={`/homes/${id}`} variant="accent">
+      <div className='sm:mt-auto'>
+        <ul className='grid grid-cols-2 font-haas_md text-lg gap-x-3 my-4'>
+          <li>${cost.toLocaleString()}</li>
+          <li>Built {yearBuilt}</li>
+          <li>{livableAreaSize} home</li>
+          <li>{bedrooms} Bed</li>
+          <li>{lotSize} lot</li>
+          <li>{bathrooms} Bath</li>
+        </ul>
+        <NavButton to={`/homes/${id}`} variant='accent'>
           View Details
         </NavButton>
       </div>
